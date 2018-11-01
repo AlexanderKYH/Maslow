d8c1eff3d1a9d36d15e26457dfd0fab9ff53b441
Mona-elrobaiy <44396801+Mona-elrobaiy@users.noreply.github.com>
2018-11-01 13:23:41 +0100

    historik

---
+++ historik.py
@@ -0,0 +1,26 @@
+class historik :
+
+    def set_level_results():
+        Average_value = total_result/nr_lines
+        print(Gamla level result)
+    def set_total_results():
+        Average_value = total_result/nr_lines
+        print(gamla total results)
+
+    def get_level_results():
+        for line in open("data.txt", "r"):
+            level_result += line
+
+
+    def get_total_results():
+        for line in open("data.txt", "r"):
+            total_result += line
+
+            return total_result
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
