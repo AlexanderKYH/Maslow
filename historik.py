50ad8abe8ebff3e95c11557dd81c4dab873f409b
Mona-elrobaiy <44396801+Mona-elrobaiy@users.noreply.github.com>
2018-11-08 15:22:02 +0100

    Update historik.py

--- historik.py
+++ historik.py
@@ -1,26 +1,45 @@
 class historik :
 
-    def set_level_results():
+    def set_level_results(self, result):
+        file = open("data.txt", "a")
+        file.write(Result)
+
+
         Average_value = total_result/nr_lines
-        print(Gamla level result)
+        print(Gamla_level_result)
+
+
     def set_total_results():
         Average_value = total_result/nr_lines
-        print(gamla total results)
+        print(gamla_total_results)
 
-    def get_level_results():
-        for line in open("data.txt", "r"):
-            level_result += line
+    def get_level_results(self):
+        file = open("data.txt", "r")
+        level_result += line
+        data = line.split(";")
+        data = line.split(" ")
 
 
-    def get_total_results():
-        for line in open("data.txt", "r"):
-            total_result += line
+    def get_total_results(self):
+        file = open("data.txt", "r")
+        total_result += line
+        data = line.split(";")
+        data = line.split(" ")
 
-            return total_result
+        print.file.read()
+
+
+
+        return total_result
 
 
 def main():
+ # file-input.py
+ f = open('data.txt', 'r')
+ message = f.read()
+ print(message)
+
 
 
 if __name__ == '__main__':
-    main()
+   main()
